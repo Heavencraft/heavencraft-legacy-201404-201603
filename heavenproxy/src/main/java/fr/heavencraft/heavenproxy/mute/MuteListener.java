db2cd4b4e41b87343fab67fd079790bc4abb1270
LRGN <lrgn@protonmail.com>
2014-04-30 23:44:33 +0200

    [Proxy] Retrait des logs sur l'event ChatEvent

--- heavenproxy/src/main/java/fr/heavencraft/heavenproxy/mute/MuteListener.java
+++ heavenproxy/src/main/java/fr/heavencraft/heavenproxy/mute/MuteListener.java
@@ -13,27 +13,27 @@ import fr.heavencraft.heavenproxy.Utils;
 public class MuteListener implements Listener
 {
 	private static final String TAG = "[MuteListener] ";
-	
+
 	private final Logger log = Utils.getLogger();
 	private final List<String> bannedWords = new ArrayList<String>();
 
 	public MuteListener()
 	{
 		Utils.registerListener(this);
-		
-		bannedWords.add("batar");	// Batard, batar
+
+		bannedWords.add("batar"); // Batard, batar
 		bannedWords.add("connass");
-		bannedWords.add("encul");	// encule, enculé, enculer
-		bannedWords.add("fuck");	// fuck, fucking, fucktard, motherfucker
-		bannedWords.add("salau");	// salaud
-		bannedWords.add("salop");	// salop, salope, salopard
+		bannedWords.add("encul"); // encule, enculé, enculer
+		bannedWords.add("fuck"); // fuck, fucking, fucktard, motherfucker
+		bannedWords.add("salau"); // salaud
+		bannedWords.add("salop"); // salop, salope, salopard
 
 		bannedWords.add("putin");
 		bannedWords.add("putain");
 		bannedWords.add("bitch");
 		bannedWords.add("biatch");
 		bannedWords.add("dumbass");
-		
+
 		log.info(TAG + "Initialized");
 	}
 
@@ -43,8 +43,8 @@ public class MuteListener implements Listener
 		if (event.isCancelled())
 			return;
 
-		log.info(TAG + event);
-		
+		// log.info(TAG + event);
+
 		if (!(event.getSender() instanceof ProxiedPlayer))
 			return;
 
@@ -55,24 +55,24 @@ public class MuteListener implements Listener
 				&& !message.startsWith("/t ") && !message.startsWith("/tell ") && !message.startsWith("/w ")
 				&& !message.startsWith("/me ") && !message.startsWith("/envoyer "))
 			return;
-		
+
 		ProxiedPlayer player = (ProxiedPlayer) event.getSender();
 		String playerName = player.getName();
-		
+
 		// Si le joueur est mute
 		if (MuteManager.isMuted(playerName))
 		{
 			event.setCancelled(true);
 			return;
 		}
-		
+
 		// Mots interdits
 		for (String bannedWord : bannedWords)
 			if (message.contains(bannedWord))
 			{
 				MuteManager.mutePlayer(playerName, 5);
 				Utils.sendMessage(player, "Vous avez été mute pour {5} minutes.");
-				
+
 				event.setCancelled(true);
 				return;
 			}
