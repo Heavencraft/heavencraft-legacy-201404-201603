95b7e293ae8148fe82f7ef54771a479d8af4daa5
Manuel R <rauscher.manuel@gmail.com>
2014-05-01 03:40:35 +0200

    + Le colis et serialisé, et inséré dans la BDD; ainsi qu'un log.

--- LaPoste/src/main/java/fr/heavencraft/laposte/handlers/Colis.java
+++ LaPoste/src/main/java/fr/heavencraft/laposte/handlers/Colis.java
@@ -1,16 +1,22 @@
 package fr.heavencraft.laposte.handlers;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
+import fr.heavencraft.laposte.InventoryUtils;
+import fr.heavencraft.laposte.LaPoste;
+
 public class Colis {
 	private Inventory contenu;
-	private Player proprietaire;
+	private Player expediteur;
 	private Player destinataire;
 	
 	public Colis(Player owner, Player dest)
 	{
-		proprietaire = owner;
+		expediteur = owner;
 		destinataire = dest;
 		
 		owner.openInventory(contenu);
@@ -20,9 +26,9 @@ public class Colis {
 	{
 		return contenu;
 	}
-	public Player getProprio()
+	public Player getExpediteur()
 	{
-		return proprietaire;
+		return expediteur;
 	}
 	public String getNom()
 	{
@@ -31,6 +37,24 @@ public class Colis {
 	
 	public void envoyer()
 	{
-		//TODO: Envoyer le colis
+		//TODO: Pour economiser de la place, mettre le string inventaire en Base 64
+		try
+		{
+			PreparedStatement ps = LaPoste.getMainConnection().prepareStatement(
+					"INSERT INTO poste_colis (expediteur, destinataire, dateEnvoi, contenu, isLOG) VALUES (?, ?, NOW(), ?, ?)");
+			ps.setString(1, expediteur.getUniqueId().toString());
+			ps.setString(2, destinataire.getUniqueId().toString());
+			ps.setString(3, InventoryUtils.InventoryToString(contenu));
+			ps.setBoolean(4, false);
+			ps.executeUpdate();
+			//inserer le log aussi
+			ps.setBoolean(4, true);
+			ps.executeUpdate();
+		}
+
+		catch (SQLException ex)
+		{
+			ex.printStackTrace();
+		}
 	}
 }
