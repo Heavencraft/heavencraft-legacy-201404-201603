98cc37c603d766bfac611b0fbd0fb01e5ebedbe6
lorgan17 <lorgan17@gmail.com>
2014-05-17 22:43:35 +0200

    [Crea] Passage à Maven et Bukkit 1.7.9

--- CreaPlugin/src/main/java/fr/tenkei/creaplugin/listeners/protection/ProtectionBlockListener.java
+++ CreaPlugin/src/main/java/fr/tenkei/creaplugin/listeners/protection/ProtectionBlockListener.java
@@ -1,12 +1,12 @@
 package fr.tenkei.creaplugin.listeners.protection;
 
-import net.minecraft.server.v1_7_R1.WorldServer;
+import net.minecraft.server.v1_7_R3.WorldServer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.craftbukkit.v1_7_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_7_R3.CraftWorld;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -23,9 +23,9 @@ import fr.tenkei.creaplugin.MyPlugin;
 import fr.tenkei.creaplugin.utils.Message;
 import fr.tenkei.creaplugin.utils.Stuff;
 
+public class ProtectionBlockListener implements Listener
+{
 
-public class ProtectionBlockListener implements Listener {
-	
 	public ProtectionBlockListener(MyPlugin plugin)
 	{
 		Bukkit.getPluginManager().registerEvents(this, plugin);
@@ -39,12 +39,12 @@ public class ProtectionBlockListener implements Listener {
 
 		if (!Stuff.canBeDestroyed(player, block))
 			event.setCancelled(true);
-		
+
 	}
-	
+
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onBlockBurn(BlockBurnEvent event)
-	{	
+	{
 		event.setCancelled(true);
 	}
 
@@ -53,13 +53,13 @@ public class ProtectionBlockListener implements Listener {
 	{
 		Player player = event.getPlayer();
 		Block block = event.getBlock();
-		
+
 		if (Stuff.canBeDestroyed(player, block))
 			return;
-		
+
 		if (!Stuff.canBeDestroyed(player, block))
 			event.setCancelled(true);
-		
+
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST)
@@ -67,14 +67,16 @@ public class ProtectionBlockListener implements Listener {
 	{
 		Player player = event.getPlayer();
 		Block block = event.getBlock();
-		
-		if(player == null) {
+
+		if (player == null)
+		{
 			event.setCancelled(true);
 			return;
 		}
-		
-		if (!Stuff.canBeDestroyed(player, block)) {
-			Message.sendMessage(player, "Cet endroit est protégé.");
+
+		if (!Stuff.canBeDestroyed(player, block))
+		{
+			Message.sendMessage(player, "Cet endroit est protï¿½gï¿½.");
 			event.setCancelled(true);
 		}
 	}
@@ -85,22 +87,23 @@ public class ProtectionBlockListener implements Listener {
 	{
 		Player player = event.getPlayer();
 		Block block = event.getBlockPlaced();
-		
-		if(block.getTypeId() == 68)
-			if(block.getRelative(BlockFace.NORTH).getTypeId() == 125
-			|| block.getRelative(BlockFace.SOUTH).getTypeId() == 125
-			|| block.getRelative(BlockFace.EAST).getTypeId() == 125
-			|| block.getRelative(BlockFace.WEST).getTypeId() == 125)
-				return; //On laisse poser
-		
-		if (!Stuff.canBeDestroyed(player, block)) {
-			Message.sendMessage(player, "Cet endroit est protégé.");
+
+		if (block.getTypeId() == 68)
+			if (block.getRelative(BlockFace.NORTH).getTypeId() == 125
+					|| block.getRelative(BlockFace.SOUTH).getTypeId() == 125
+					|| block.getRelative(BlockFace.EAST).getTypeId() == 125
+					|| block.getRelative(BlockFace.WEST).getTypeId() == 125)
+				return; // On laisse poser
+
+		if (!Stuff.canBeDestroyed(player, block))
+		{
+			Message.sendMessage(player, "Cet endroit est protï¿½gï¿½.");
 			event.setCancelled(true);
 		}
 
-		
-		if (block.getTypeId() == 144 || block.getTypeId() == 397) {
-			//player.getEquipment().setItemInHand(new ItemStack(Material.AIR));
+		if (block.getTypeId() == 144 || block.getTypeId() == 397)
+		{
+			// player.getEquipment().setItemInHand(new ItemStack(Material.AIR));
 			ItemStack itemInHand = player.getEquipment().getItemInHand();
 			itemInHand.setAmount(itemInHand.getAmount() - 1);
 			player.getEquipment().setItemInHand(itemInHand);
@@ -111,15 +114,17 @@ public class ProtectionBlockListener implements Listener {
 
 		if (b.getTypeId() != 123)
 			return;
-		
-		WorldServer ws = ((CraftWorld)b.getWorld()).getHandle();
+
+		WorldServer ws = ((CraftWorld) b.getWorld()).getHandle();
 
 		boolean mem = ws.isStatic;
-		if (!mem) ws.isStatic = true;
+		if (!mem)
+			ws.isStatic = true;
 
-		b.setTypeIdAndData(Material.REDSTONE_LAMP_ON.getId(), (byte)0, false);
-		
-		if (!mem) ws.isStatic = false;
+		b.setTypeIdAndData(Material.REDSTONE_LAMP_ON.getId(), (byte) 0, false);
+
+		if (!mem)
+			ws.isStatic = false;
 	}
 
 	@SuppressWarnings("deprecation")
@@ -129,15 +134,16 @@ public class ProtectionBlockListener implements Listener {
 		Player player = event.getPlayer();
 		Block block = event.getBlock();
 
-		if(block.getType().getId() == 68)
-			if(block.getRelative(BlockFace.NORTH).getTypeId() == 125
-			|| block.getRelative(BlockFace.SOUTH).getTypeId() == 125
-			|| block.getRelative(BlockFace.EAST).getTypeId() == 125
-			|| block.getRelative(BlockFace.WEST).getTypeId() == 125)
+		if (block.getType().getId() == 68)
+			if (block.getRelative(BlockFace.NORTH).getTypeId() == 125
+					|| block.getRelative(BlockFace.SOUTH).getTypeId() == 125
+					|| block.getRelative(BlockFace.EAST).getTypeId() == 125
+					|| block.getRelative(BlockFace.WEST).getTypeId() == 125)
 				return;
-		
-		if (!Stuff.canBeDestroyed(player, block)) {
-			Message.sendMessage(player, "Cet endroit est protégé.");
+
+		if (!Stuff.canBeDestroyed(player, block))
+		{
+			Message.sendMessage(player, "Cet endroit est protï¿½gï¿½.");
 			event.setCancelled(true);
 		}
 	}
