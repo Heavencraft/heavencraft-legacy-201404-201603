2e693b83672201654d92dcfea802118b8373e976
LRGN <lrgn@protonmail.com>
2014-05-19 21:25:22 +0200

    [Créa] Passage de la limite à 1500 blocs suite à la demande d'ABibiche

--- CreaPlugin/src/main/java/fr/tenkei/creaplugin/MyPlugin.java
+++ CreaPlugin/src/main/java/fr/tenkei/creaplugin/MyPlugin.java
@@ -10,59 +10,62 @@ import fr.tenkei.creaplugin.listeners.ListenersManager;
 import fr.tenkei.creaplugin.managers.ManagersManager;
 import fr.tenkei.creaplugin.utils.ConnectionManager;
 
-
-public class MyPlugin extends JavaPlugin {
+public class MyPlugin extends JavaPlugin
+{
 
 	public final static String pluginTag = "[Plugin-Creative] ";
 	public final static String builder = "server.builder";
 	public final static String archiModo = "server.archimodo";
 	public final static String administrator = "server.administrator";
-	public final static int LIMITE_WORLDS = 1200;
-	
+	public final static int LIMITE_WORLDS = 1500;
+
 	private static Logger _log = Logger.getLogger("Minecraft");
-	
+
 	private ManagersManager _myManagers;
-	
-	
+
 	@Override
 	public void onEnable()
 	{
 		super.onEnable();
-        
+
 		// Connexion DB
 		ConnectionManager.enableConnection();
-		
+
 		// Gestionnaire de Manager
 		_myManagers = new ManagersManager(this);
-		
-        // Gestionnaire de commandes
+
+		// Gestionnaire de commandes
 		new CommandsManager(this);
-		
-		 // Gestionnaire de listeners
+
+		// Gestionnaire de listeners
 		new ListenersManager(this);
 
 		_log.info(pluginTag + " " + getDescription().getVersion() + " created by " + getDescription().getAuthors());
 	}
-	
+
 	@Override
 	public void onDisable()
-	{	
-		
+	{
+
 		try
 		{
 			_myManagers.getUserManager().saveAllUsers();
 			Bukkit.getScheduler().cancelTasks(this);
 		}
-		catch (Exception ex){ ex.printStackTrace(); }
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
 	}
-	
-	public ManagersManager getManagers() {
+
+	public ManagersManager getManagers()
+	{
 		return _myManagers;
-		
+
 	}
-	
-	public static void log(String str) {
+
+	public static void log(String str)
+	{
 		_log.info(pluginTag + " " + str);
 	}
 }
-
