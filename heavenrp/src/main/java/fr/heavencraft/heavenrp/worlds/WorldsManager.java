4892b13976186935e2944dfb0010c3c18d8064e1
LRGN <lrgn@protonmail.com>
2014-05-31 15:46:54 +0200

    [SRP] Reformatage

--- heavenrp/src/main/java/fr/heavencraft/heavenrp/worlds/WorldsManager.java
+++ heavenrp/src/main/java/fr/heavencraft/heavenrp/worlds/WorldsManager.java
@@ -1,7 +1,5 @@
 package fr.heavencraft.heavenrp.worlds;
 
-import fr.heavencraft.Utils;
-
 import java.util.Random;
 
 import org.bukkit.Bukkit;
@@ -10,14 +8,16 @@ import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.block.Biome;
 
+import fr.heavencraft.Utils;
+
 public class WorldsManager
 {
 	public static final int RESOURCES_SIZE = 5000;
-	
+
 	private static Location _spawn;
 	private static Location _spawnNether;
 	private static Location _spawnTheEnd;
-	
+
 	private static Location _tuto;
 	static Random rnd = new Random();
 
@@ -34,7 +34,7 @@ public class WorldsManager
 		{
 			WorldCreator creator = new WorldCreator("world_resources");
 			creator.environment(World.Environment.NORMAL);
-			//creator.seed(9139863690993604117l);
+			// creator.seed(9139863690993604117l);
 			creator.createWorld();
 		}
 
@@ -52,7 +52,7 @@ public class WorldsManager
 			creator.environment(World.Environment.NORMAL);
 			creator.createWorld();
 		}
-		
+
 		if (!isLoaded("world_origine"))
 		{
 			WorldCreator creator = new WorldCreator("world_origine");
@@ -61,10 +61,10 @@ public class WorldsManager
 			creator.createWorld();
 		}
 
-		_spawn			= new Location(getWorld(), 145.5D, 67D, 130.5D, 270F, 0F);
-		_spawnNether	= new Location(getNether(), 86D, 63D, 101D, 235F, 0F);
-		_spawnTheEnd	= new Location(getTheEnd(), 4.5D, 67D, 23.5D, 0F, 0F);
-		_tuto			= new Location(getWorld(), -818D, 35D, -728D, 0F, 0F);
+		_spawn = new Location(getWorld(), 145.5D, 67D, 130.5D, 270F, 0F);
+		_spawnNether = new Location(getNether(), 106D, 54D, 150D, 235F, 0F);
+		_spawnTheEnd = new Location(getTheEnd(), 4.5D, 67D, 23.5D, 0F, 0F);
+		_tuto = new Location(getWorld(), -818D, 35D, -728D, 0F, 0F);
 	}
 
 	public static Location getSpawn()
@@ -95,8 +95,8 @@ public class WorldsManager
 		{
 			x = rnd.nextInt(5000) - 2500;
 			z = rnd.nextInt(5000) - 2500;
-		} while ((getResources().getBiome(x, z) == Biome.OCEAN)
-				|| (getResources().getBiome(x, z) == Biome.DEEP_OCEAN));
+		}
+		while ((getResources().getBiome(x, z) == Biome.OCEAN) || (getResources().getBiome(x, z) == Biome.DEEP_OCEAN));
 
 		return Utils.getSafeDestination(new Location(getResources(), x, 100.0D, z));
 	}
