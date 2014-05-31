97341a92e795d1dc5741823fde39273f12a41d53
LRGN <lrgn@protonmail.com>
2014-05-31 15:32:59 +0200

    [Proxy] Modification de la détection des mots interdits

--- heavenproxy/src/main/java/fr/heavencraft/heavenproxy/mute/MuteListener.java
+++ heavenproxy/src/main/java/fr/heavencraft/heavenproxy/mute/MuteListener.java
@@ -21,19 +21,63 @@ public class MuteListener implements Listener
 	{
 		Utils.registerListener(this);
 
-		bannedWords.add("batar"); // Batard, batar
-		bannedWords.add("connass");
-		bannedWords.add("encul"); // encule, enculé, enculer
-		bannedWords.add("fuck"); // fuck, fucking, fucktard, motherfucker
-		bannedWords.add("salau"); // salaud
-		bannedWords.add("salop"); // salop, salope, salopard
-
-		bannedWords.add("merde");
-		bannedWords.add("putin");
-		bannedWords.add("putain");
-		bannedWords.add("bitch");
+		// Insultes
+		bannedWords.add("batard");
 		bannedWords.add("biatch");
+		bannedWords.add("bitch");
+		bannedWords.add("bounioul");
+		bannedWords.add("calice");
+		bannedWords.add("câlice");
+		bannedWords.add("caliss");
+		bannedWords.add("calisse");
+		bannedWords.add("catin");
+		bannedWords.add("ciboire");
+		bannedWords.add("conasse");
+		bannedWords.add("connard");
+		bannedWords.add("connasse");
+		bannedWords.add("cris");
+		bannedWords.add("crisse");
 		bannedWords.add("dumbass");
+		bannedWords.add("encule");
+		bannedWords.add("enculé");
+		bannedWords.add("enculer");
+		bannedWords.add("fdp");
+		bannedWords.add("fuck");
+		bannedWords.add("fucking");
+		bannedWords.add("fucktard");
+		bannedWords.add("gtfo");
+		bannedWords.add("katin");
+		bannedWords.add("lopette");
+		bannedWords.add("marde");
+		bannedWords.add("merde");
+		bannedWords.add("merdeux");
+		bannedWords.add("motherfucker");
+		bannedWords.add("niquer");
+		bannedWords.add("osti");
+		bannedWords.add("pd");
+		bannedWords.add("pédé");
+		bannedWords.add("putain");
+		bannedWords.add("putin");
+		bannedWords.add("pute");
+		bannedWords.add("salaud");
+		bannedWords.add("salop");
+		bannedWords.add("salopard");
+		bannedWords.add("salope");
+		bannedWords.add("stfu");
+		bannedWords.add("tabarnac");
+		bannedWords.add("tabarnak");
+		bannedWords.add("tabarnaque");
+		bannedWords.add("tbk");
+		bannedWords.add("tg");
+		bannedWords.add("tageule");
+		bannedWords.add("tayeul");
+		bannedWords.add("tayeule");
+
+		// Serveurs
+		bannedWords.add("desticraft");
+		bannedWords.add("historycraft");
+		bannedWords.add("minefield");
+		bannedWords.add("thecraft");
 
 		log.info(TAG + "Initialized");
 	}
@@ -66,14 +110,15 @@ public class MuteListener implements Listener
 		}
 
 		// Mots interdits
-		for (String bannedWord : bannedWords)
-			if (message.contains(bannedWord))
-			{
-				MuteManager.mutePlayer(playerName, 5);
-				Utils.sendMessage(player, "Vous avez été mute pour {5} minutes par {le Prof. Chen}.");
+		for (String word : message.split(" "))
+			for (String bannedWord : bannedWords)
+				if (word.equalsIgnoreCase(bannedWord))
+				{
+					MuteManager.mutePlayer(playerName, 5);
+					Utils.sendMessage(player, "Vous avez été mute pour {5} minutes par {le Prof. Chen}.");
 
-				event.setCancelled(true);
-				return;
-			}
+					event.setCancelled(true);
+					return;
+				}
 	}
 }
