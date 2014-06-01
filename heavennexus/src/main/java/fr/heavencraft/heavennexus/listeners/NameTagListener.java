378153a8e8d9c5ea9a8836c69b6348c01fe0ecce
lorgan17 <lorgan17@gmail.com>
2014-06-01 07:33:09 +0200

    [Nexus] Désactivation TagAPI

--- heavennexus/src/main/java/fr/heavencraft/heavennexus/listeners/NameTagListener.java
+++ heavennexus/src/main/java/fr/heavencraft/heavennexus/listeners/NameTagListener.java
@@ -1,12 +1,8 @@
 package fr.heavencraft.heavennexus.listeners;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.kitteh.tag.AsyncPlayerReceiveNameTagEvent;
 
 public class NameTagListener implements Listener {
 
@@ -14,20 +10,20 @@ public class NameTagListener implements Listener {
 	{
 		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
-	
-	@EventHandler
-	public void onNameTag(AsyncPlayerReceiveNameTagEvent event)
-	{
-		Player player = event.getNamedPlayer();
-		
-		if (!player.hasPermission("heavencraft.accueil") || player.isOp())
-			return;
-		
-		String playerName = player.getName();
-		
-		if (playerName.length() > 14)
-			return;
-		
-		event.setTag(ChatColor.GOLD + playerName);
-	}
+
+	// @EventHandler
+	// public void onNameTag(AsyncPlayerReceiveNameTagEvent event)
+	// {
+	// Player player = event.getNamedPlayer();
+	//
+	// if (!player.hasPermission("heavencraft.accueil") || player.isOp())
+	// return;
+	//
+	// String playerName = player.getName();
+	//
+	// if (playerName.length() > 14)
+	// return;
+	//
+	// event.setTag(ChatColor.GOLD + playerName);
+	// }
 }
