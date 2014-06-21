8f10bc5ff826db2d2c44de690e286b4e097efec3
lorgan17 <lorgan17@gmail.com>
2014-06-21 16:04:05 +0200

    [Créative] Problème d'accents (encore)

--- CreaPlugin/src/main/java/fr/tenkei/creaplugin/commands/user/teleport/RejoindreCommand.java
+++ CreaPlugin/src/main/java/fr/tenkei/creaplugin/commands/user/teleport/RejoindreCommand.java
@@ -10,49 +10,58 @@ import fr.tenkei.creaplugin.exceptions.MyException;
 import fr.tenkei.creaplugin.managers.entities.User;
 import fr.tenkei.creaplugin.utils.Message;
 
-public class RejoindreCommand extends Command{
+public class RejoindreCommand extends Command
+{
 
-	public RejoindreCommand(MyPlugin plugin) {
+	public RejoindreCommand(MyPlugin plugin)
+	{
 		super("rejoindre", plugin);
 	}
 
 	@Override
-	protected void onPlayerCommand(Player player, String[] args)
-			throws MyException {
+	protected void onPlayerCommand(Player player, String[] args) throws MyException
+	{
 
-		if (args.length != 1){
+		if (args.length != 1)
+		{
 			sendUsage(player);
 			return;
 		}
 
 		Player otherPlayer = Bukkit.getPlayer(args[0]);
 
-		if (otherPlayer == null) {
+		if (otherPlayer == null)
+		{
 			Message.sendMessage(player, "Le joueur {" + args[0] + "} n'existe pas.");
 			return;
 		}
 
-		if (otherPlayer.getWorld() != player.getWorld()){
-			Message.sendMessage(player, "Le joueur {" + otherPlayer.getDisplayName() + "} n'est pas dans le mÃªme monde que vous.");
+		if (otherPlayer.getWorld() != player.getWorld())
+		{
+			Message.sendMessage(player, "Le joueur {" + otherPlayer.getDisplayName()
+					+ "} n'est pas dans le même monde que vous.");
 			return;
 		}
 
 		User otherUser = getUser(otherPlayer.getName());
 		otherUser.setTeleportRequestName(player.getName());
 
-		Message.sendMessage(otherPlayer, "Le joueur {" + player.getDisplayName() + "} souhaite se tÃ©lÃ©porter vers vous. Tapez {/accepter} pour accepter.");
-		Message.sendMessage(player, "Une invitation de tÃ©lÃ©portation a Ã©tÃ© envoyÃ©e vers {" + otherPlayer.getDisplayName() + "}.");
+		Message.sendMessage(otherPlayer, "Le joueur {" + player.getDisplayName()
+				+ "} souhaite se téléporter vers vous. Tapez {/accepter} pour accepter.");
+		Message.sendMessage(player,
+				"Une invitation de téléportation a été envoyée vers {" + otherPlayer.getDisplayName() + "}.");
 	}
 
 	@Override
-	protected void onConsoleCommand(CommandSender sender, String[] args)
-			throws MyException {
+	protected void onConsoleCommand(CommandSender sender, String[] args) throws MyException
+	{
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	protected void sendUsage(CommandSender sender) {
+	protected void sendUsage(CommandSender sender)
+	{
 		Message.sendMessage(sender, "{/rejoindre} <joueur>");
 	}
 }
