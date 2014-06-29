e992f47413672fe7d23be5c08182f77f8c2d2a54
LRGN <lrgn@protonmail.com>
2014-06-29 22:33:11 +0200

    [Musée] Passage au HeavenCore + modif du gameplay

--- heavenmuseum/src/main/java/fr/heavencraft/heavenmuseum/HeavenMuseum.java
+++ heavenmuseum/src/main/java/fr/heavencraft/heavenmuseum/HeavenMuseum.java
@@ -1,26 +1,28 @@
 package fr.heavencraft.heavenmuseum;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.plugin.java.JavaPlugin;
-
+import fr.heavencraft.HeavenPlugin;
+import fr.heavencraft.commands.AccepterCommand;
+import fr.heavencraft.commands.RejoindreCommand;
+import fr.heavencraft.heavenmuseum.commands.SpawnCommand;
+import fr.heavencraft.heavenmuseum.listeners.PlayerListener;
 import fr.heavencraft.heavenmuseum.listeners.ServerListener;
 import fr.heavencraft.heavenmuseum.managers.WorldsManager;
 
-public class HeavenMuseum extends JavaPlugin
+public class HeavenMuseum extends HeavenPlugin
 {
 	@Override
 	public void onEnable()
 	{
 		super.onEnable();
 
-		new ServerListener(this);
+		new PlayerListener();
+		new ServerListener();
+
+		new SpawnCommand();
+		new RejoindreCommand();
+		new AccepterCommand();
 
 		WorldsManager.init();
 	}
 
-	public static Location getSpawn()
-	{
-		return new Location(Bukkit.getWorld("world"), 0.5, 4, 0.5);
-	}
 }
