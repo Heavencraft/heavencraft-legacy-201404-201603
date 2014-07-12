f19f969516ecca72dcf63950f3b36aaed00ca172
lorgan17 <lorgan17@gmail.com>
2014-07-12 15:30:24 +0200

    [HeavenRPG] Premier jet du système d'artefact

---
+++ HeavenRPG/src/main/java/fr/heavencraft/rpg/artifacts/effects/DamageEffect.java
@@ -0,0 +1,26 @@
+package fr.heavencraft.rpg.artifacts.effects;
+
+import org.bukkit.entity.Damageable;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import fr.heavencraft.exceptions.HeavenException;
+
+public class DamageEffect extends TargetEffect
+{
+	private final double _damage;
+
+	public DamageEffect(EffectTarget target, int radius, double damage) throws HeavenException
+	{
+		super(target, radius);
+		_damage = damage;
+	}
+
+	@Override
+	public void doEffect(Player player) throws HeavenException
+	{
+		for (Entity entity : getTargetEntities(player))
+			if (entity instanceof Damageable)
+				((Damageable) entity).damage(_damage, player);
+	}
+}
