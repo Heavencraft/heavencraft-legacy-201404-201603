a6c825047bc05f52cd94a70919eb02760ed9744c
LRGN <lrgn@protonmail.com>
2014-08-02 08:16:38 +0200

    [Fun] Adding HeavenFun plugin

---
+++ heavenfun/src/main/java/fr/heavencraft/heavenfun/ServerListener.java
@@ -0,0 +1,28 @@
+package fr.heavencraft.heavenfun;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import fr.heavencraft.utils.DevUtil;
+
+public class ServerListener implements Listener
+{
+	public ServerListener()
+	{
+		DevUtil.registerListener(this);
+	}
+
+	@EventHandler
+	private void onPlayerRespawn(PlayerRespawnEvent event)
+	{
+		event.setRespawnLocation(HeavenFun.SPAWN);
+	}
+
+	@EventHandler
+	private void onPlayerLogin(PlayerLoginEvent event)
+	{
+		event.getPlayer().teleport(HeavenFun.SPAWN);
+	}
+}
