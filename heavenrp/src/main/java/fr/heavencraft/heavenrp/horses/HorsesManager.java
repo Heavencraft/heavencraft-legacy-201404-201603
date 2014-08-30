f16170c3c865663aabdaab80562b822462e35b63
lorgan17 <lorgan17@gmail.com>
2014-08-30 16:54:02 +0200

    [SRP] A horse with a null owner is now unuseable, and must be unlock by a moderator by using /cheval donner <player>

--- heavenrp/src/main/java/fr/heavencraft/heavenrp/horses/HorsesManager.java
+++ heavenrp/src/main/java/fr/heavencraft/heavenrp/horses/HorsesManager.java
@@ -10,7 +10,7 @@ public class HorsesManager
 {
 	public static boolean isWild(Horse horse)
 	{
-		return horse.getOwner() == null;
+		return !horse.isTamed();
 	}
 
 	public static boolean canUse(Horse horse, Player player)
@@ -19,12 +19,18 @@ public class HorsesManager
 			return true;
 		else if (isWild(horse))
 			return true;
+		else if (horse.getOwner() == null)
+			return false;
 		else
 			return horse.getOwner().getUniqueId().equals(player.getUniqueId());
 	}
 
 	public static void sendWarning(Horse horse, Player player)
 	{
-		ChatUtil.sendMessage(player, "Ce cheval appartient à {%1$s}.", horse.getOwner().getName());
+		if (horse.getOwner() == null)
+			ChatUtil.sendMessage(player,
+					"Ce cheval est apprivoisé mais n'a pas de propriétaire. Merci de contacter un administrateur.");
+		else
+			ChatUtil.sendMessage(player, "Ce cheval appartient à {%1$s}.", horse.getOwner().getName());
 	}
 }
