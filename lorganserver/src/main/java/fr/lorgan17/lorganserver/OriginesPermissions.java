386766db66d6b95f092b8596ad256e5d6e5ce977
LRGN <lrgn@protonmail.com>
2014-08-31 18:15:14 +0200

    [Origines] Add bypass protections for moderators

--- lorganserver/src/main/java/fr/lorgan17/lorganserver/OriginesPermissions.java
+++ lorganserver/src/main/java/fr/lorgan17/lorganserver/OriginesPermissions.java
@@ -4,4 +4,7 @@ public class OriginesPermissions
 {
 
 	public static final String SETSPAWN = "heavencraft.commands.setspawn";
+
+	public static final String PROTECTION_BYPASS = "heavencraft.protections.bypass";
+
 }
