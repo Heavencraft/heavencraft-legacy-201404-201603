1007a716bef0b3a72f6fa23ca821ab307bed86d0
LRGN <lrgn@protonmail.com>
2014-10-19 16:25:04 +0200

    [Crea] Add /jetons donner

--- CreaPlugin/src/main/java/fr/tenkei/creaplugin/commands/user/JetonsCommand.java
+++ CreaPlugin/src/main/java/fr/tenkei/creaplugin/commands/user/JetonsCommand.java
@@ -9,6 +9,8 @@ import org.bukkit.entity.Player;
 import fr.heavencraft.commands.HeavenCommand;
 import fr.heavencraft.exceptions.HeavenException;
 import fr.heavencraft.utils.ChatUtil;
+import fr.heavencraft.utils.DevUtil;
+import fr.heavencraft.utils.PlayerUtil;
 import fr.tenkei.creaplugin.users.UserProvider;
 
 public class JetonsCommand extends HeavenCommand
@@ -22,14 +24,33 @@ public class JetonsCommand extends HeavenCommand
 	protected void onPlayerCommand(Player player, String[] args) throws HeavenException
 	{
 		if (args.length == 0)
-			ChatUtil.sendMessage(player, "Vous avez {" + UserProvider.getUserByName(player.getName()).getJetons()
-					+ "} jetons sur vous.");
+		{
+			ChatUtil.sendMessage(player, "Vous avez {%1$s} jetons sur vous.",
+					UserProvider.getUserByName(player.getName()).getJetons());
+			return;
+		}
+
+		switch (args[0])
+		{
+			case "donner":
+				if (args.length == 3)
+				{
+					String dest = PlayerUtil.getExactName(args[1]);
+					int delta = DevUtil.toUint(args[2]);
+
+					UserProvider.getUserByName(player.getName()).updateBalance(-delta);
+					UserProvider.getUserByName(dest).updateBalance(delta);
+
+					ChatUtil.sendMessage(player, "Vous avez donné {%1$s} jetons à {%2$s}.", delta, dest);
+				}
+				break;
+		}
 	}
 
 	@Override
 	protected void onConsoleCommand(CommandSender sender, String[] args) throws HeavenException
 	{
-		if(args.length != 3)
+		if (args.length != 3)
 			return;
 		UserProvider.getUserByName(args[1]).updateBalance(Integer.parseInt(args[2]));
 		Bukkit.getLogger().log(Level.INFO, "Solde mis a jour");
