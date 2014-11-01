b723b8576b19ffebfcf3df07b4263ed788888ecb
Manuel R <rauscher.manuel@gmail.com>
2014-11-01 01:49:33 +0100

    [Halloween] Correctifs mineurs

--- Halloween2014/src/main/java/fr/heavencraft/heavenhallow/levels/Level.java
+++ Halloween2014/src/main/java/fr/heavencraft/heavenhallow/levels/Level.java
@@ -6,7 +6,7 @@ import org.bukkit.Location;
 public enum Level {
 	
 	LOBBY("Lobby", new Location(Bukkit.getWorld("halloween"), -285, 6, -1348)),
-	THEBEGIN("La Tour Antique", new Location(Bukkit.getWorld("halloween"), -364, 150, -1038)),
+	THEBEGIN("La Tour Antique", new Location(Bukkit.getWorld("halloween"), -365, 147, -1038)),
 	THEFOREST("Misty Forest", new Location(Bukkit.getWorld("halloween"), -382, 10, -1148)),
 	THETIGER("Le Tigre Blanc", new Location(Bukkit.getWorld("halloween"), -308, 17, -1381)),
 	THEMOON("La Lune", new Location(Bukkit.getWorld("halloween"), -377, 61, -1438)),
--- Halloween2014/src/main/java/fr/heavencraft/heavenhallow/player/HallowlayerListener.java
+++ Halloween2014/src/main/java/fr/heavencraft/heavenhallow/player/HallowlayerListener.java
@@ -1,17 +1,22 @@
 package fr.heavencraft.heavenhallow.player;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 import fr.heavencraft.HeavenHallow;
 
@@ -24,6 +29,9 @@ public class HallowlayerListener implements Listener {
 	// When a player joins the server, create a RPGPlayer for them
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onJoinCreateHallowPlayer(PlayerJoinEvent e) {
+		e.setJoinMessage("");
+		e.getPlayer().setGameMode(GameMode.SURVIVAL);
+		e.getPlayer().setFlying(false);
 		Player p = e.getPlayer();
 		HallowPlayer IP = new HallowPlayer(p);
 		HallowPlayerManager.createHallowPlayer(IP);
@@ -32,6 +40,7 @@ public class HallowlayerListener implements Listener {
 	// When a player leaves the server willingly, delete the RPGPlayer of them
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onLeaveDeleteHallowPlayer(final PlayerQuitEvent e) {
+		e.setQuitMessage("");
 		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(HeavenHallow.getInstance(), new Runnable()
 		{
 			@Override
@@ -86,6 +95,28 @@ public class HallowlayerListener implements Listener {
 			e.setCancelled(true);
 		}
 	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onPlayerDamageByEntity(EntityDamageByBlockEvent e) {
+		if (!(e.getEntity() instanceof Player))
+			return;	
+
+		Player victim = (Player) e.getEntity();		
+
+		if (victim.getHealth() - e.getDamage() <= 0)
+		{
+			e.setDamage(0);
+			HallowPlayerManager.handlePlayerRespawn(victim);
+			e.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onPlayerDeath(PlayerDeathEvent e) {
+		e.setDroppedExp(0);
+		e.setDeathMessage("");
+	}
+
 
 	@EventHandler public void onFoodLevelChange(FoodLevelChangeEvent event){
 		if (event.isCancelled()) 
