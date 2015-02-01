1baca62605e728620f5b858f6f49a327e0fa8af1
lorgan17 <lorgan17@gmail.com>
2015-02-01 14:03:07 +0100

    [Proxy] Add minecraft: aliases

--- heavenproxy/src/main/java/fr/heavencraft/heavenproxy/commands/MeCommand.java
+++ heavenproxy/src/main/java/fr/heavencraft/heavenproxy/commands/MeCommand.java
@@ -6,12 +6,13 @@ import net.md_5.bungee.api.chat.TextComponent;
 import fr.heavencraft.heavenproxy.Utils;
 import fr.heavencraft.heavenproxy.exceptions.HeavenException;
 
-public class MeCommand extends HeavenCommand {
+public class MeCommand extends HeavenCommand
+{
 	private final static String ME_MESSAGE = " * %1$s %2$s";
-	
+
 	public MeCommand()
 	{
-		super("me", "", new String[] {});
+		super("me", "", new String[] { "minecraft:me" });
 	}
 
 	@Override
@@ -22,11 +23,11 @@ public class MeCommand extends HeavenCommand {
 			sendUsage(sender);
 			return;
 		}
-		
-		String message = String.format(ME_MESSAGE, sender.getName(), Utils.ArrayToString(args, 0, " "));
+
+		final String message = String.format(ME_MESSAGE, sender.getName(), Utils.ArrayToString(args, 0, " "));
 		ProxyServer.getInstance().broadcast(TextComponent.fromLegacyText(message));
 	}
-	
+
 	private void sendUsage(CommandSender sender)
 	{
 		Utils.sendMessage(sender, "/{me} <message>");
