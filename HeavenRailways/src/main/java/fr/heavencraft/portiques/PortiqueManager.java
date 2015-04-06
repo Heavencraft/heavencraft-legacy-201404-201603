b320541dbf1ff277805cfc6339dab1e50500b05d
Manuel R <rauscher.manuel@gmail.com>
2015-04-06 02:52:11 +0200

    [Railways] Les joueurs seronts chargé pour passer un portique.

--- HeavenRailways/src/main/java/fr/heavencraft/portiques/PortiqueManager.java
+++ HeavenRailways/src/main/java/fr/heavencraft/portiques/PortiqueManager.java
@@ -16,7 +16,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import fr.heavencraft.Utils.BankAccountsManager;
+import fr.heavencraft.Utils.BankAccountsManager.BankAccount;
 import fr.heavencraft.Utils.ChatUtil;
+import fr.heavencraft.Utils.User;
+import fr.heavencraft.Utils.UserProvider;
+import fr.heavencraft.exceptions.HeavenException;
 import fr.heavencraft.railways.HeavenRailway;
 
 public class PortiqueManager implements Listener
@@ -26,7 +31,11 @@ public class PortiqueManager implements Listener
 	public static HashMap<String, Portique> portiquesOccupes = new HashMap<String, Portique>();
 	private final static String YOU_HAVE_BEEN_CHARGED = "[Portique] Vous avez été chargé de {%1$d} PO(s).";
 	private final static String PORTIQUE_INVALID_ACCOUNT = "[Portique] Le compte a crediter est invalide.";
+	private final static String PORTIQUE_INVALID_ACCOUNT_FORMAT = "[Portique] Le compte a crediter est dans le mauvait format: <U/T/W>:<nom>.";
+	private final static String PORTIQUE_INVALID_ACCOUNT_TYPE = "[Portique] Le type de compte a crediter est invalide: U (user) / T (town) / E (entreprise).";
 	private final static String PORTIQUE_INVALID_PRICE = "[Portique] Le prix est invalide.";
+	private final static String PORTIQUE_NO_MONEY = "[Portique] Vous n'avez pas assez d'argent.";
+	private final static String PORTIQUE_UNKNOWN_USER = "[Portique] Erreur inconnue, vous ne semblez pas exiter...";
 	
 	public PortiqueManager()
 	{
@@ -43,19 +52,77 @@ public class PortiqueManager implements Listener
 		if(params == null) 
 			return;
 		
-		String account = params.getLine(1);
-		if(account.trim().equalsIgnoreCase("")) {
+		String account[] = params.getLine(1).split(":");
+		
+		BankAccount creditedAccount = null;
+		
+		if(account.length != 2) {
+			ChatUtil.sendMessage(event.getPlayer(), PORTIQUE_INVALID_ACCOUNT_FORMAT);
+			return;
+		} else {
+			if(!account[0].equalsIgnoreCase("U") && !account[0].equalsIgnoreCase("T") && !account[0].equalsIgnoreCase("E")) {
+				ChatUtil.sendMessage(event.getPlayer(), PORTIQUE_INVALID_ACCOUNT_TYPE);
+				return;
+			}
+			try
+			{
+				creditedAccount = BankAccountsManager.getBankAccount(account[1], BankAccountsManager.BankAccountType.getByCode(account[0]));
+			}
+			catch (HeavenException e)
+			{
+				ChatUtil.sendMessage(event.getPlayer(), PORTIQUE_INVALID_ACCOUNT);
+				return;
+			}
+		}
+		
+		if(creditedAccount == null) {
 			ChatUtil.sendMessage(event.getPlayer(), PORTIQUE_INVALID_ACCOUNT);
 			return;
 		}
+		
+		//TODO verifier que l'argent est bien débité
+		User sender = null;
+		try
+		{
+			sender = UserProvider.getUserByName(event.getPlayer().getName());
+		}
+		catch (HeavenException e1)
+		{
+			e1.printStackTrace();
+			ChatUtil.sendMessage(event.getPlayer(), PORTIQUE_UNKNOWN_USER);
+			return;
+		}
+		if(sender == null)
+		{
+			ChatUtil.sendMessage(event.getPlayer(), PORTIQUE_UNKNOWN_USER);
+			return;
+		}
+		
 		if(params.getLine(2).trim().equalsIgnoreCase("")) {
 			ChatUtil.sendMessage(event.getPlayer(), PORTIQUE_INVALID_PRICE);
 			return;
 		}
+		
 		int price = Math.abs(Integer.parseInt(params.getLine(2)));
+		if(sender.getBalance() <= price)
+		{
+			ChatUtil.sendMessage(event.getPlayer(), PORTIQUE_NO_MONEY);
+			return;
+		}
 		
 		if(price >= 0) {
 			ChatUtil.sendMessage(event.getPlayer(), YOU_HAVE_BEEN_CHARGED, price);
+			try
+			{
+				sender.updateBalance(-price);
+				creditedAccount.updateBalance(price);
+			}
+			catch (HeavenException e)
+			{
+				e.printStackTrace();
+				ChatUtil.sendMessage(event.getPlayer(), PORTIQUE_INVALID_ACCOUNT);
+				return;
+			}
 		}
 		
 		Portique fenceGateportique = new Portique(block.getLocation(), block.getType());
