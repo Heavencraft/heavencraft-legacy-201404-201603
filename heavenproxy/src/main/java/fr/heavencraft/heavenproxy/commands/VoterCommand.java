53a155af8e5cd8e888964b606718d24e5a0d232c
lorgan17 <lorgan17@gmail.com>
2015-11-21 23:14:46 +0100

    Proxy: Update /voter command

--- heavenproxy/src/main/java/fr/heavencraft/heavenproxy/commands/VoterCommand.java
+++ heavenproxy/src/main/java/fr/heavencraft/heavenproxy/commands/VoterCommand.java
@@ -18,6 +18,9 @@ public class VoterCommand extends HeavenCommand
 		Utils.sendMessage(sender, ChatColor.GREEN + "Vous pouvez nous aider en votant et commentant :");
 		Utils.sendMessage(sender, ChatColor.GREEN + "http://www.mcserv.org/Heavencraftfr_3002.html 1 fois/6h");
 		Utils.sendMessage(sender, ChatColor.GREEN + "http://mc-topserv.net/top/serveur.php?serv=46 1 fois/j");
+		Utils.sendMessage(sender, ChatColor.GREEN + "http://top-serveurs-minecraft.fr/fiche_serveur.php?id=1345");
+		Utils.sendMessage(sender, ChatColor.GREEN + "http://www.serveursminecraft.org/serveur.php?id=104");
+		Utils.sendMessage(sender, ChatColor.GREEN + "http://www.rpg-paradize.com/site-Heavencraft-104357");
 	}
 
 }
