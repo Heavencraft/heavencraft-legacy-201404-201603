733af418a4a88169676c7760d62232dff43b47ab
LRGN <lrgn@protonmail.com>
2016-03-02 23:20:02 +0900

    HeavenProxy

--- heavenproxy/src/main/java/fr/heavencraft/heavenproxy/ban/UnbanCommand.java
+++ heavenproxy/src/main/java/fr/heavencraft/heavenproxy/ban/UnbanCommand.java
@@ -1,36 +1,34 @@
 package fr.heavencraft.heavenproxy.ban;
 
-import net.md_5.bungee.api.CommandSender;
 import fr.heavencraft.heavenproxy.Utils;
 import fr.heavencraft.heavenproxy.commands.HeavenCommand;
+import fr.heavencraft.heavenproxy.database.users.UserProvider;
 import fr.heavencraft.heavenproxy.exceptions.HeavenException;
-import fr.heavencraft.heavenproxy.listeners.LogListener;
+import net.md_5.bungee.api.CommandSender;
 
 public class UnbanCommand extends HeavenCommand
 {
-	public UnbanCommand()
-	{
-		super("unban", "heavencraft.commands.unban", new String[] { "gunban" });
-	}
+    public UnbanCommand()
+    {
+        super("unban", "heavencraft.commands.unban", new String[] { "gunban" });
+    }
 
-	@Override
-	protected void onCommand(CommandSender sender, String[] args) throws HeavenException
-	{
-		switch (args.length)
-		{
-			case 1:
-				BanManager.unbanPlayer(args[0]);
-				Utils.sendMessage(sender, "Le joueur {%1$s} a été débanni d'Heavencraft.", args[0]);
-				LogListener.addUnban(args[0], sender.getName());
-				break;
-			default:
-				sendUsage(sender);
-				break;
-		}
-	}
+    @Override
+    protected void onCommand(CommandSender sender, String[] args) throws HeavenException
+    {
+        switch (args.length)
+        {
+            case 1:
+                BanManager.unbanPlayer(UserProvider.getUserByName(args[0]), sender);
+                break;
+            default:
+                sendUsage(sender);
+                break;
+        }
+    }
 
-	private void sendUsage(CommandSender sender)
-	{
-		Utils.sendMessage(sender, "/{unban} <joueur>");
-	}
+    private void sendUsage(CommandSender sender)
+    {
+        Utils.sendMessage(sender, "/{unban} <joueur>");
+    }
 }
